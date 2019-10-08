ee601af3bbfd6caf6dbc83797fdb2fd6a93660dc
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2019-10-08 17:14:34 -0300

    Refactor RepositoryValidator

--- app/validators/repository_validator.rb
+++ app/validators/repository_validator.rb
@@ -1,8 +1,10 @@
 class RepositoryValidator < ActiveModel::Validator
   def validate(record)
-    unless record.user.github_username.downcase == record.url.split("/")[-2].downcase
-      record.errors[:ownership] << 'you must be the owner of this repo in order to add it'
-    end
+    username = record.user.github_username
+    repo_owner = record.url.split('/')[-2]
+    return if username.downcase == repo_owner.downcase
+
+    record.errors[:ownership] <<
+      'you must be the owner of this repo in order to add it'
   end
 end
- 
