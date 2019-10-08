90af9bacbcb5371d9488db9e98067c92055edd26
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2019-10-08 13:03:44 -0300

    Extract message sending from WebhookController

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -8,8 +8,7 @@ class WebhooksController < ApplicationController
         NotifierIssue.new(Issue.find_or_create_by(url: issue_params[:html_url]))
       )
       issue.sync_with_webhook_payload!(issue_params)
-      owner = issue.repository.user
-      TwilioUpdateMessage.new(issue).send(owner) if owner.phone_number
+      issue.send_message!
       issue.send_email!
     end
     head :no_content
--- app/models/notifier_issue.rb
+++ app/models/notifier_issue.rb
@@ -2,4 +2,9 @@ class NotifierIssue < SimpleDelegator
   def send_email!
     UserMailer.issue_update_email(user, self).deliver_now
   end
+
+  def send_message!
+    owner = repository.user
+    TwilioUpdateMessage.new(self).send(owner) if owner.phone_number
+  end
 end
