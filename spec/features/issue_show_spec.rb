ea8646e3a7bce16d0f578d319f808b85bee7c311
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2019-10-08 21:53:48 -0300

    Add issues factory into specs

--- spec/features/issue_show_spec.rb
+++ spec/features/issue_show_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe Issue, type: :feature do
   let(:user) { create(:user) }
   let(:repo) { create(:repository, user: user) }
+  let!(:issue) { create(:issue, repository: repo) }
   before(:each) do
-    @issue = Issue.create(title: 'my issue', content: 'this is a test issue.', repository: repo, opened_on: DateTime.now, url: 'https://github.com/SophieDeBenedetto/learn-write/issues/1')
     sign_in
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
   end
 
   describe '#show' do
     it "displays an issues's title, content, github link and repo name" do
-      visit issue_path(@issue)
+      visit issue_path(issue)
       expect(page).to have_content('my issue')
       expect(page).to have_content('this is a test issue.')
       expect(page).to have_content('view on github')
@@ -20,16 +20,16 @@ RSpec.describe Issue, type: :feature do
 
     context 'without an assignee' do
       it "displays 'n/a' for the assignee atttribute" do
-        visit issue_path(@issue)
+        visit issue_path(issue)
         expect(page).to have_content('assignee: n/a')
       end
     end
 
     context 'with an assignee' do
       it 'displays the name of the person assigned for the assignee atttribute' do
-        @issue.assignee = 'antoinfive'
-        @issue.save
-        visit issue_path(@issue)
+        issue.assignee = 'antoinfive'
+        issue.save
+        visit issue_path(issue)
         expect(page).to have_content('assignee: antoinfive')
       end
     end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -4,12 +4,10 @@ RSpec.describe UserMailer, type: :mailer do
   describe '#issue_update_email' do
     let(:user) { create(:user) }
     let(:repo) { create(:repository, user: user) }
-    before(:each) do
-      @issue = Issue.create(title: 'my issue', content: 'this is a test issue.', repository: repo, opened_on: DateTime.now, url: 'https://github.com/SophieDeBenedetto/learn-write/issues/1')
-    end
+    let(:issue) { create(:issue, repository: repo) }
 
     it 'sends an email to a user when an issue on one of their repos has been created or changed' do
-      email = UserMailer.issue_update_email(user, @issue).deliver_now
+      email = UserMailer.issue_update_email(user, issue).deliver_now
       expect(ActionMailer::Base.deliveries.length).to eq(1)
       expect(email.from).to eq(['ghissue.trackr@gmail.com'])
       expect(email.to).to eq(['sophie.debenedetto@gmail.com'])
