f1fffc9daa74e7404ef0fd7aad2a91805ebc6ed0
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2019-10-08 22:01:03 -0300

    Remove last instantiation from before block

--- spec/controllers/webhooks_controller_spec.rb
+++ spec/controllers/webhooks_controller_spec.rb
@@ -7,14 +7,17 @@ RSpec.describe WebhooksController, type: :controller do
   describe '#receive' do
     let(:user) { create(:user, :with_phone_number) }
     let!(:repo) { create(:repository, user: user) }
-    before(:each) do
-      @payload = JSON.parse(File.read("#{FIXTURES_PATH}/webhook_issue_payload.json"), symbolize_names: true)
+    let(:payload) do
+      JSON.parse(
+        File.read("#{FIXTURES_PATH}/webhook_issue_payload.json"),
+        symbolize_names: true
+      )
     end
 
     context 'with a new issue' do
       it 'creates a new issue with the info in the payload from github' do
         VCR.use_cassette('webooks_receive') do
-          post :receive, params: @payload
+          post :receive, params: payload
           issue = Issue.first
           expect(repo.issues.count).to eq(1)
           expect(issue.url).to eq('https://github.com/SophieDeBenedetto/learn-write/issues/5')
@@ -27,7 +30,7 @@ RSpec.describe WebhooksController, type: :controller do
     context 'updating an existing issue' do
       it 'updates an existing issue with the info in the payload from github' do
         repo.issues.build(url: 'https://github.com/SophieDeBenedetto/learn-write/issues/5', content: 'testing')
-        post :receive, params: @payload
+        post :receive, params: payload
         expect(repo.issues.count).to eq(1)
         expect(repo.issues.first.content).to eq('THIS IS a TEST')
       end
@@ -35,7 +38,7 @@ RSpec.describe WebhooksController, type: :controller do
 
     context 'notifications' do
       it 'sends an email update to the user whose issue has been created/updated' do
-        post :receive, params: @payload
+        post :receive, params: payload
         email = ActionMailer::Base.deliveries.last
         expect(ActionMailer::Base.deliveries.length).to eq(1)
         expect(email.from).to eq(['ghissue.trackr@gmail.com'])
@@ -44,7 +47,7 @@ RSpec.describe WebhooksController, type: :controller do
       end
 
       it 'sends a text message to the use whoe issue has been created/updated' do
-        post :receive, params: @payload
+        post :receive, params: payload
         text_message = open_last_text_message_for(user.phone_number)
         expect(text_message.body).to eq('test issue 2 has been updated. View it here: https://github.com/SophieDeBenedetto/learn-write/issues/5')
         expect(text_message.from).to eq('+1 914-363-0827')
